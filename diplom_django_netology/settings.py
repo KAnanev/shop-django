93b82e9ba505ededcc51c8067b5d96d281a6f815
KAnanev <russrage@gmail.com>
2021-04-05 17:37:36 +0300

    secret_key move environment

--- diplom_django_netology/settings.py
+++ diplom_django_netology/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx=7law(fuponyu-wz3dy#-%3@%zoph&$*7f+9r#3vk0ot7z-_'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
